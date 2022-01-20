aee13a7da196a614a23995ae294cfc3a2616c8d4
Marcelus <kz_@hotmail.es>
2022-01-20 16:19:28 +0100

    Reduce done

--- 02-js_reduce.js
+++ 02-js_reduce.js
@@ -1,1 +1,66 @@
 // Class code examples
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const sum = numbers.reduce(
+
+    (accumulator, currentElement) => {
+        accumulator += currentElement
+        return accumulator  // MUST ALWAYS return the accumulator
+    },
+    0
+    // Starting value of the accumulator is teh second parameter
+)
+
+console.log(sum);
+
+const students = [
+    {
+        firstName: 'John',
+        lastName: 'Carr',
+        age: 34
+    },
+    {
+        firstName: 'Leonardo',
+        lastName: 'Di Vittorio',
+        age: 21
+    },
+    {
+        firstName: 'Jessy',
+        lastName: "Mc Scrudge",
+        age: 99
+    },
+    {
+        firstName: 'Lisa',
+        lastName: "Parker",
+        age: 22
+    },
+    {
+        firstName: 'Marco',
+        lastName: "Santo",
+        age: 48
+    }
+]
+
+
+
+
+const summary = students.reduce(
+    (count, student) => {
+        if (student.age > 30) return count += 1
+        else return count
+    },
+    0
+)
+
+console.log(summary);
+
+const classRoster = students.reduce(
+    (roster, student)=>{
+        roster.push(`${student.firstName} ${student.lastName}`)
+        return roster
+    }
+    ,
+    []
+)
+
+console.log(classRoster);
--- exercises/01-exe-js_map.js
+++ exercises/01-exe-js_map.js
@@ -4,8 +4,9 @@
 // Exercise 1: Capitalize each element of the array - the whole word:
 const fruits = ['pineapple', 'orange', 'mango'];
 
-let uppercaseFruits = fruits.map(() => {
-  //your code here...
+let uppercaseFruits = fruits.map((string) => {
+   //your code here...
+   return string.toUpperCase()
 })
 console.log(uppercaseFruits)
 
@@ -15,30 +16,34 @@ console.log(uppercaseFruits)
 // Exercise 2: Capitalize the first letter of every city. 
 const cities = ['miami', 'barcelona', 'madrid', 'amsterdam', 'berlin', 'sao paulo', 'lisbon', 'mexico city']
 
-let uppercaseCities // your code here...
+let upperCaseCities = cities.map((string) => {
+   return string[0].toUpperCase() + string.slice(1)
+})
+
+console.log(upperCaseCities);
 
 // Answer should be 
 // [ 'Miami', 'Barcelona', 'Madrid', 'Amsterdam', 'Berlin', 'Sao paulo', 'Lisbon', 'Mexico city', 'Paris' ]
 
 // Exercise  3: Create an array with only the names of the restaurants 
 // Note: Be careful here. Info is an object with a key called 'cities' which is an array
- 
+
 const info = {
-    cities : [
-      {name: 'Mirazur', owner : 'Gustavo'},
-      {name: 'Noma', owner : 'Jorge'},
-      {name: 'Gaggan', owner : 'Alvaro'},
-      {name: 'Geranium', owner : 'Gustavo'},
-      {name: 'Arpège', owner : 'Luis'},    
-   ]  
+   cities: [
+      { name: 'Mirazur', owner: 'Gustavo' },
+      { name: 'Noma', owner: 'Jorge' },
+      { name: 'Gaggan', owner: 'Alvaro' },
+      { name: 'Geranium', owner: 'Gustavo' },
+      { name: 'Arpège', owner: 'Luis' },
+   ]
 }
 
 
 const getRestaurantsNames = (arr) => {
    //your code here...
+   return arr.cities.forEach((el) => { console.log(el.name) })
 }
 
-let restaurantsNames = getRestaurantsNames( /* pass your array here*/)
-console.log(restaurantsNames)
+let restaurantsNames = getRestaurantsNames(info)
 
 // Answer should be [ 'Mirazur', 'Noma', 'Gaggan', 'Geranium', 'Arpège']
